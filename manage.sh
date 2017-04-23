42cb9924f528b7244fb812753b841d967d421efe
Payam Mohajeri <payam_p70@yahoo.com>
2017-04-24 00:19:24 +0200

    meteor tutorials

--- manage.sh
+++ manage.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 function install {
-    curl https://install.meteor.com/ | sh;
+    curl https://install.meteor.com/ | sh
     # sudo apt-get install npm;
     sudo apt-get install python-software-properties
     curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
     sudo apt-get install nodejs
-    
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    sudo apt-get install -y mongodb-org
 
 }
 
