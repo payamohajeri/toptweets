9fe0c32eaa0ada146f05ebbe20b9e006d2bb3e62
Payam Mohajeri <payam_p70@yahoo.com>
2017-04-24 15:36:54 +0200

    removed config

--- tweets/imports/twitter/config.js
+++
@@ -1,7 +0,0 @@
-{
-  "consumer_key":"EkYoMsWOVtwzhIQP0J4rZ8QBx",
-  "consumer_secret":"dU5BH19Ru8DSOCHQ5SFkWIJMTpBepHePvC8TTi7f8PHYmWIDbK",
-  "access_token":"756337523081801732-pVKgjn0h2RzAKGA8A3gqiVGbY6zKX1P",
-  "access_token_secret":"iHXyEXJpdwE4kTQzTKMLqnwbiC5Zi2hhN1qln3XUmsSKH",
-  "timeout_ms":60*1000
-}
